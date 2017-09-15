e350a6502d822a2d6015e5534783d3b09ba51ed9
Antonije Ivanovic <antonije999@gmail.com>
2017-09-15 19:49:48 +0200

    Fixed tracer handshake and started work on anounnce request.

--- goTit/goTit.go
+++ goTit/goTit.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
-//	"io/ioutil"
-//	"github.com/anivanovic/goTit/metainfo"
-//	"crypto/sha1"
+	"bytes"
+	"strings"
+	"io/ioutil"
+	"github.com/anivanovic/goTit/metainfo"
+	"crypto/sha1"
 	"net"
+	"net/url"
 	"encoding/binary"
-	"strconv"
-//	"bufio"
+	"time"
+	"math/rand"
 )
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func CheckError(err error) {
     if err  != nil {
@@ -17,51 +22,101 @@ func CheckError(err error) {
     }
 }
 
+func randStringBytes(n int) []byte {
+    b := make([]byte, n)
+    for i := range b {
+        b[i] = letterBytes[rand.Intn(len(letterBytes))]
+    }
+    return b
+}
+
 func main() {
-//	file, _ := ioutil.ReadFile("C:/Users/Antonije/Downloads/Allied (2016) [1080p] [YTS.AG].torrent")
-//	fmt.Println("-------------------------------------------------------------------------------------")
-//	_, printStr := metainfo.Decode(string(file), "", "")
-//	fmt.Println(printStr)
-//	
-//	info, _ := ioutil.ReadFile("C:/Users/Antonije/Downloads/info.txt")
-//	sha := sha1.New()
-//	sha.Write([]byte(string(info)))
-//	
-//	hash := sha.Sum(nil)
-//	fmt.Printf("info hash: %x\n", hash)
+	file, _ := ioutil.ReadFile("C:/Users/Antonije/Downloads/Alien- Covenant (2017) [720p] [YTS.AG].torrent")
+	fmt.Println("-------------------------------------------------------------------------------------")
+	torrent := string(file)
+	_, printStr := metainfo.Decode(torrent, "", "")
+	fmt.Println(printStr)
 	
-	udpAddr, err := net.ResolveUDPAddr("udp", "tracker.openbittorrent.com:80")
+	infoDict := torrent[strings.Index(torrent, "4:info")+6:len(torrent)-1]
+	sha := sha1.New()
+	sha.Write([]byte(string(infoDict)))
+	
+	var hash []byte
+	hash = sha.Sum(nil)
+	fmt.Printf("info hash: %x\n", hash)
+
+	u, err := url.Parse("udp://p4p.arenabg.com:1337")
 	CheckError(err)
 	
-	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	udpAddr, err := net.ResolveUDPAddr("udp", u.Host)
+	fmt.Println("Connecting to. " + u.Host)
+	CheckError(err)
+    
+    Conn, err := net.ListenUDP("udp", &net.UDPAddr{Port: 6679})
     CheckError(err)
     
-    Conn, err := net.DialUDP("udp", LocalAddr, udpAddr)
-    CheckError(err)
-    
-	defer Conn.Close()
+//	defer Conn.Close()
 	
-	p :=  make([]byte, 2048)
-	data := make([]byte, 16)
-	action := uint32(0)
+	Conn.SetDeadline(time.Now().Add(time.Second * time.Duration(60)))
 	
-	connection_id, _ := strconv.ParseInt("41727101980", 16, 64)
-	uconnection_id := uint64(connection_id)
-	transaction_id := uint32(123986378)
-	binary.BigEndian.PutUint64(data, uconnection_id)
-	binary.BigEndian.PutUint32(data, action)
-	binary.BigEndian.PutUint32(data, transaction_id)
+	request := new(bytes.Buffer)
+	p := make([]byte, 16)
 	
-	i, err := Conn.Write(data)
-	fmt.Println("sent data to udp tracker: " + string(i))
+	var action uint32 = 0
+	var connection_id uint64 = 0x41727101980
+	transaction_id := uint32(12398636)
+	
+	binary.Write(request, binary.BigEndian, connection_id)
+	binary.Write(request, binary.BigEndian, action)
+	binary.Write(request, binary.BigEndian, transaction_id)
+	
+	Conn.WriteTo(request.Bytes(), udpAddr)
+	length, _, err := Conn.ReadFromUDP(p)
+//	i, err := Conn.Write(request.Bytes())
+//	fmt.Printf("sent data to udp tracker: %d \n", i)
 
 	CheckError(err)
 	
-	red, err := Conn.Read(p)
+//	length, err := Conn.Read(p)
 	fmt.Println("read response")
-	if red == 16 {
+	if length == 16 {
 		fmt.Println("Read 16 bites")
 	}
-	fmt.Println("read " + string(red))
-	fmt.Printf("%s\n", p)
+	connVar := binary.BigEndian.Uint32(p[:4])
+	transResp := binary.BigEndian.Uint32(p[4:8])
+	connId := binary.BigEndian.Uint64(p[8:16])
+	fmt.Println("rsponse: ", connVar, transResp, connId)
+	
+	request = new(bytes.Buffer)
+	binary.Write(request, binary.BigEndian, connVar)
+	binary.Write(request, binary.BigEndian, uint32(1))
+	binary.Write(request, binary.BigEndian, uint32(127545))
+	binary.Write(request, binary.BigEndian, hash)
+	peer_id := randStringBytes(20)
+	binary.Write(request, binary.BigEndian, peer_id)
+	binary.Write(request, binary.BigEndian, uint64(0))
+	binary.Write(request, binary.BigEndian, uint64(960989559))
+	binary.Write(request, binary.BigEndian, uint64(0))
+	binary.Write(request, binary.BigEndian, uint32(0))
+	binary.Write(request, binary.BigEndian, uint32(0))
+	randKey := rand.Int31()
+	binary.Write(request, binary.BigEndian, randKey)
+	binary.Write(request, binary.BigEndian, int32(-1))
+	binary.Write(request, binary.BigEndian, uint16(6679))
+	
+	Conn.WriteTo(request.Bytes(), udpAddr)
+	fmt.Println("Send request")
+	response := make([]byte, 0, 4096)
+	tmp := make([]byte, 256)
+	
+	fmt.Println("reading")
+	length, _, err = Conn.ReadFromUDP(tmp)
+	response = append(response, tmp[:length]...)
+	CheckError(err)
+	fmt.Println("READ")
+	fmt.Println("Dohvaćeno podataka ", len(response))
+	
+	resCode := binary.BigEndian.Uint32(response[:4])
+	message := string(response[8:])
+	fmt.Print("response code ", resCode, message)
 }
