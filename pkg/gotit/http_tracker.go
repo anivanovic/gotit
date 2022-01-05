3e84a4e2c6ea14dd5e85b276610a3656d8aeb9e5
Antonije Ivanovic <aivanovic@squarespace.com>
2022-01-05 17:58:14 +0000

    Handle both binary and bencode dict peers.
    In http tracker before we could only handle raw byte peers.
    Now we can also handle peers response as bencode dict.

--- pkg/gotit/http_tracker.go
+++ pkg/gotit/http_tracker.go
@@ -2,6 +2,8 @@ package gotit
 
 import (
 	"context"
+	"encoding/binary"
+	"net"
 	"net/http"
 	"net/url"
 
@@ -11,17 +13,12 @@ import (
 
 	"errors"
 
-	"encoding/binary"
-	"net"
-
 	"github.com/anivanovic/gotit/pkg/bencode"
 	log "github.com/sirupsen/logrus"
 )
 
 type http_tracker struct {
-	Url              *url.URL
-	AnnounceInterval int
-	Ips              *map[string]bool
+	Url *url.URL
 }
 
 func httpTracker(url *url.URL) Tracker {
@@ -31,7 +28,7 @@ func httpTracker(url *url.URL) Tracker {
 	return t
 }
 
-func (t *http_tracker) Announce(ctx context.Context, torrent *Torrent) (map[string]struct{}, error) {
+func (t http_tracker) Announce(ctx context.Context, torrent *Torrent) (map[string]struct{}, error) {
 	query := t.Url.Query()
 	query.Set("info_hash", string(torrent.Hash))
 	query.Set("peer_id", string(torrent.PeerId))
@@ -42,16 +39,20 @@ func (t *http_tracker) Announce(ctx context.Context, torrent *Torrent) (map[stri
 	query.Set("compact", "1")
 	t.Url.RawQuery = query.Encode()
 
-	res, err := http.Get(t.Url.String())
-
+	r, err := http.NewRequestWithContext(ctx, http.MethodGet, t.Url.String(), nil)
 	if err != nil {
-		CheckError(err)
 		return nil, err
 	}
 
+	res, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return nil, err
+	}
 	defer res.Body.Close()
 	data, err := ioutil.ReadAll(res.Body)
-	CheckError(err)
+	if err != nil {
+		return nil, err
+	}
 
 	body := string(data)
 	logger := log.WithFields(log.Fields{
@@ -64,31 +65,68 @@ func (t *http_tracker) Announce(ctx context.Context, torrent *Torrent) (map[stri
 		return nil, errors.New("http tracker returned response code " + strconv.Itoa(res.StatusCode))
 	}
 
-	logger.WithField("body", body).Debug("Http tracker announce response")
-	benc, _ := bencode.Parse(body)
-	dict := benc[0]
-	ips := readHttpAnnounce(dict)
-	return ips, nil
+	logger.Debug("Http tracker announce response")
+	benc, err := bencode.Parse(body)
+	if err != nil {
+		return nil, err
+	}
+	return readPeers(benc[0])
 }
 
 func (t *http_tracker) Close() error { return nil }
 
-func readHttpAnnounce(elem bencode.Bencode) map[string]struct{} {
+func readPeers(elem bencode.Bencode) (map[string]struct{}, error) {
 	if benDict, ok := elem.(bencode.DictElement); ok {
-		peers := benDict.Value("peers").String()
-		ipData := []byte(peers)
-		size := len(ipData)
-		peerCount := size / 6
-		ips := make(map[string]struct{})
-		for read := 0; read < peerCount; read++ {
-			byteMask := 6
-
-			ipAddress := net.IPv4(ipData[byteMask*read], ipData[byteMask*read+1], ipData[byteMask*read+2], ipData[byteMask*read+3])
-			port := binary.BigEndian.Uint16(ipData[byteMask*read+4 : byteMask*read+6])
-			ipAddr := ipAddress.String() + ":" + strconv.Itoa(int(port))
-			ips[ipAddr] = struct{}{}
+		peers := benDict.Value("peers")
+		if peers == nil {
+			return nil, errors.New("tracker: no peers in announce response")
 		}
-		return ips
+
+		switch peers := peers.(type) {
+		case bencode.ListElement:
+			return parseListPeers(peers), nil
+		case bencode.StringElement:
+			return parseStringPeers(peers), nil
+		default:
+			return nil, errors.New("tracker: announce peers of wrong type")
+		}
 	}
-	return nil
+
+	return nil, errors.New("tracker: no peers in announce response")
+}
+
+func parseListPeers(peers bencode.ListElement) map[string]struct{} {
+	ips := make(map[string]struct{})
+	for _, p := range peers {
+		data, ok := p.(bencode.DictElement)
+		if !ok {
+			continue
+		}
+
+		ip := data.Value("ip").String()
+		peerId := data.Value("peer id")
+		port := data.Value("port").String()
+		ipAddr := ip + ":" + port
+		log.Debugf("ip: %s, port: %s, peerId: %s", ip, port, peerId)
+		ips[ipAddr] = struct{}{}
+	}
+	return ips
+}
+
+func parseStringPeers(peers bencode.StringElement) map[string]struct{} {
+	ipData := []byte(peers.String())
+	peerCount := len(ipData) / 6
+
+	byteMask, ips := 6, make(map[string]struct{})
+	for read := 0; read < peerCount; read++ {
+		ipAddress := net.IPv4(
+			ipData[byteMask*read],
+			ipData[byteMask*read+1],
+			ipData[byteMask*read+2],
+			ipData[byteMask*read+3])
+		port := binary.BigEndian.Uint16(ipData[byteMask*read+4 : byteMask*read+6])
+		ipAddr := ipAddress.String() + ":" + strconv.Itoa(int(port))
+		ips[ipAddr] = struct{}{}
+	}
+	return ips
 }
